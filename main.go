3e26b569f0f6040590e2c2a5022980a1df5460cb
Andrew Arrow <andrew@bird.co>
2020-06-15 20:38:12 -0700

    links

--- main.go
+++ main.go
@@ -16,6 +16,21 @@ func main() {
 
 	if os.Args[1] == "--list" {
 
+		// https://www.terraform.io/docs/providers/do/r/droplet.html
+		// https://www.terraform.io/docs/providers/do/r/loadbalancer.html
+
+		// https://www.terraform.io/docs/providers/vultr/r/server.html
+		// https://www.terraform.io/docs/providers/vultr/r/load_balancer.html
+
+		// https://www.terraform.io/docs/providers/aws/r/instance.html
+		// https://www.terraform.io/docs/providers/aws/r/lb.html
+
+		// https://www.terraform.io/docs/providers/google/r/compute_instance.html
+		// https://www.terraform.io/docs/providers/google/r/compute_target_pool.html
+
+		// https://www.terraform.io/docs/providers/linode/r/instance.html
+		// https://www.terraform.io/docs/providers/linode/r/nodebalancer.html
+
 		fmt.Println("")
 		fmt.Println("1. DigitalOcean")
 		fmt.Println("2. Vultr")

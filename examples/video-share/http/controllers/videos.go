b7d2278e998e7eb51644bd737bd5ac2acb5613d0
Andrew Arrow <andrew@bird.co>
2020-06-29 01:46:34 -0700

    util

--- examples/video-share/http/controllers/videos.go
+++ examples/video-share/http/controllers/videos.go
@@ -2,14 +2,11 @@ package controllers
 
 import (
 	"net/http"
-	"os"
-	"os/exec"
 	"regexp"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"jjaa.me/models"
-	"jjaa.me/util"
 )
 
 func VideosNew(c *gin.Context) {
@@ -93,42 +90,46 @@ func VideosDestroy(c *gin.Context) {
 	c.Abort()
 }
 func VideosFile(c *gin.Context) {
-	BeforeAll("", c)
-	video, _ := models.SelectVideo(Db, c.Param("name"))
-	file, _ := c.FormFile("file")
-	tokens := strings.Split(file.Filename, ".")
-	ext := tokens[len(tokens)-1]
-	fileWithExt := video.UrlSafeName + "." + ext
-	c.SaveUploadedFile(file, util.AllConfig.Path.Videos+fileWithExt)
-	models.UpdateVideo(Db, "uploaded", video.UrlSafeName)
-	go convertVideoFile(fileWithExt, video.UrlSafeName)
-	c.Redirect(http.StatusFound, "/")
-	c.Abort()
+	/*
+		BeforeAll("", c)
+		video, _ := models.SelectVideo(Db, c.Param("name"))
+		file, _ := c.FormFile("file")
+		tokens := strings.Split(file.Filename, ".")
+		ext := tokens[len(tokens)-1]
+		fileWithExt := video.UrlSafeName + "." + ext
+		c.SaveUploadedFile(file, util.AllConfig.Path.Videos+fileWithExt)
+		models.UpdateVideo(Db, "uploaded", video.UrlSafeName)
+		go convertVideoFile(fileWithExt, video.UrlSafeName)
+		c.Redirect(http.StatusFound, "/")
+		c.Abort()
+	*/
 }
 func convertVideoFile(fileWithExt, filename string) {
-	//ffmpeg -ss 00:00:03 -i input -vframes 1 -q:v 2 output.jpg
-	exec.Command("ffmpeg", "-ss", "00:00:03", "-i",
-		util.AllConfig.Path.Videos+fileWithExt,
-		"-vframes", "1", "-q:v", "2",
-		util.AllConfig.Path.Videos+filename+".jpg").Output()
-	models.UpdateVideo(Db, "jpg_ready", filename)
-	exec.Command("ffmpeg", "-i",
-		util.AllConfig.Path.Videos+fileWithExt,
-		"-vcodec", "h264", "-acodec", "aac",
-		util.AllConfig.Path.Videos+filename+".mp4").Output()
-	models.UpdateVideo(Db, "mp4_ready", filename)
-	exec.Command("ffmpeg", "-i",
-		util.AllConfig.Path.Videos+fileWithExt,
-		util.AllConfig.Path.Videos+filename+".webm").Output()
-	models.UpdateVideo(Db, "webm_ready", filename)
-	exec.Command("ffmpeg", "-i",
-		util.AllConfig.Path.Videos+fileWithExt,
-		util.AllConfig.Path.Videos+filename+".m4a").Output()
-	models.UpdateVideo(Db, "m4a_ready", filename)
-	exec.Command("ffmpeg", "-i",
-		util.AllConfig.Path.Videos+fileWithExt,
-		util.AllConfig.Path.Videos+filename+".oga").Output()
+	/*
+		//ffmpeg -ss 00:00:03 -i input -vframes 1 -q:v 2 output.jpg
+		exec.Command("ffmpeg", "-ss", "00:00:03", "-i",
+			util.AllConfig.Path.Videos+fileWithExt,
+			"-vframes", "1", "-q:v", "2",
+			util.AllConfig.Path.Videos+filename+".jpg").Output()
+		models.UpdateVideo(Db, "jpg_ready", filename)
+		exec.Command("ffmpeg", "-i",
+			util.AllConfig.Path.Videos+fileWithExt,
+			"-vcodec", "h264", "-acodec", "aac",
+			util.AllConfig.Path.Videos+filename+".mp4").Output()
+		models.UpdateVideo(Db, "mp4_ready", filename)
+		exec.Command("ffmpeg", "-i",
+			util.AllConfig.Path.Videos+fileWithExt,
+			util.AllConfig.Path.Videos+filename+".webm").Output()
+		models.UpdateVideo(Db, "webm_ready", filename)
+		exec.Command("ffmpeg", "-i",
+			util.AllConfig.Path.Videos+fileWithExt,
+			util.AllConfig.Path.Videos+filename+".m4a").Output()
+		models.UpdateVideo(Db, "m4a_ready", filename)
+		exec.Command("ffmpeg", "-i",
+			util.AllConfig.Path.Videos+fileWithExt,
+			util.AllConfig.Path.Videos+filename+".oga").Output()
 
-	models.UpdateVideo(Db, "live", filename)
-	os.Remove(util.AllConfig.Path.Videos + fileWithExt)
+		models.UpdateVideo(Db, "live", filename)
+		os.Remove(util.AllConfig.Path.Videos + fileWithExt)
+	*/
 }

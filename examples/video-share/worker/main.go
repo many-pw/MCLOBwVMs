4a97a8a465506f1c4ed2630661952977c2e09b33
Andrew Arrow <andrew@bird.co>
2020-06-29 17:19:24 -0700

    jpg

--- examples/video-share/worker/main.go
+++ examples/video-share/worker/main.go
@@ -6,32 +6,37 @@ import (
 	"os/exec"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/tjarratt/babble"
 	"jjaa.me/models"
 	"jjaa.me/persist"
 )
 
 var babbler = babble.NewBabbler()
+var Db *sqlx.DB
 
 func main() {
 	fmt.Println("worker")
 	rand.Seed(time.Now().UnixNano())
-	Db := persist.Connection()
+	Db = persist.Connection()
 	babbler.Count = 4
 	name := babbler.Babble()
 	for {
 		video, _ := models.SelectVideoForWorker(Db, name)
 		fmt.Println("got", video)
-		filename := video.UrlSafeName
 
-		DownloadFromBucket(filename + "." + video.Ext)
+		DownloadFromBucket(video.UrlSafeName + "." + video.Ext)
 
-		exec.Command("ffmpeg", "-ss", "00:00:03", "-i",
-			filename+"."+video.Ext,
-			"-vframes", "1", "-q:v", "2",
-			filename+".jpg").Run()
-		models.UpdateVideo(Db, "jpg_ready", video.Ext, filename)
+		ExtractJpg(video)
 
 		models.ClearVideoForWorker(Db, name)
 	}
+}
+
+func ExtractJpg(video *models.Video) {
+	exec.Command("ffmpeg", "-ss", "00:00:03", "-i",
+		video.UrlSafeName+"."+video.Ext,
+		"-vframes", "1", "-q:v", "2",
+		video.UrlSafeName+".jpg").Run()
+	models.UpdateVideo(Db, "jpg_ready", video.Ext, video.UrlSafeName)
 }
